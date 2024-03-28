3d5c5cfdc4cbb3b43cc08f65127452340e529450
Nick Wogan <nicholaswogan@gmail.com>
2024-03-27 18:40:58 -0700

    first commit

---
+++ forwarddif.f90
@@ -0,0 +1,183 @@
+module forwarddif
+  use iso_fortran_env, only: dp => real64
+  implicit none
+  private
+
+  public :: derivative, fcn_sig
+  
+  public :: dp
+  public :: dual
+  public :: sin, cos
+  public :: operator(**)
+  public :: operator(*), operator(/)
+  public :: operator(+)
+  
+  type :: dual
+    real(dp) :: val
+    real(dp), allocatable :: der(:)
+  end type
+  
+  interface dual
+    module procedure :: create_dual_1
+  end interface
+  
+  interface sin
+    module procedure :: sin_d
+  end interface
+  
+  interface cos
+    module procedure :: cos_d
+  end interface
+  
+  interface operator (**)
+    module procedure :: pow_dr
+  end interface
+  
+  interface operator (*)
+    module procedure :: mul_dd
+    module procedure :: mul_dr
+    module procedure :: mul_rd
+  end interface
+  
+  interface operator (/)
+    module procedure :: div_dd
+    module procedure :: div_dr
+    module procedure :: div_rd
+  end interface
+  
+  interface operator (+)
+    module procedure :: add_dd
+    module procedure :: add_dr
+    module procedure :: add_rd
+  end interface
+  
+  ! interface operator (-)
+  !   module procedure :: sub_dd
+  !   module procedure :: sub_dr
+  !   module procedure :: sub_rd
+  ! end interface
+
+  abstract interface
+    function fcn_sig(x) result(res)
+      import :: dual
+      type(dual), intent(in) :: x
+      type(dual) :: res
+    end function
+  end interface
+  
+contains
+
+  subroutine derivative(fcn, x, fcn_x, dfcn_x)
+    procedure(fcn_sig) :: fcn
+    real(dp), intent(in) :: x
+    real(dp), intent(out) :: fcn_x
+    real(dp), intent(out) :: dfcn_x
+    type(dual) :: f
+    f = fcn(dual(x))
+    fcn_x = f%val
+    dfcn_x = f%der(1)
+  end subroutine
+
+  pure elemental function create_dual_1(val) result(b)
+    real(dp), intent(in) :: val
+    type(dual) :: b
+    b%val = val
+    b%der = [1.0_dp]
+  end function
+  
+  pure elemental function sin_d(a) result(b)
+    type(dual), intent(in) :: a
+    type(dual) :: b
+    b%val = sin(a%val)
+    b%der = a%der*cos(a%val)
+  end function
+  
+  pure elemental function cos_d(a) result(b)
+    type(dual), intent(in) :: a
+    type(dual) :: b
+    b%val = cos(a%val)
+    b%der = -a%der*sin(a%val)
+  end function
+  
+  pure elemental function pow_dr(a1, a2) result(b)
+    type(dual), intent(in) :: a1
+    real(dp), intent(in) :: a2
+    type(dual) :: b
+    b%val = a1%val**a2
+    b%der = a2*(a1%val**(a2-1))*a1%der
+  end function
+  
+  pure elemental function mul_dd(a1, a2) result(b)
+    type(dual), intent(in) :: a1
+    type(dual), intent(in) :: a2
+    type(dual) :: b
+    b%val = a1%val*a2%val
+    b%der = a1%val*a2%der + a1%der*a2%val
+  end function
+  
+  pure elemental function mul_dr(a1, a2) result(b)
+    type(dual), intent(in) :: a1
+    real(dp), intent(in) :: a2
+    type(dual) :: b
+    b%val = a1%val*a2
+    b%der = a1%der*a2
+  end function
+  
+  pure elemental function mul_rd(a1, a2) result(b)
+    real(dp), intent(in) :: a1
+    type(dual), intent(in) :: a2
+    type(dual) :: b
+    b%val = a1*a2%val
+    b%der = a1*a2%der
+  end function
+  
+  pure elemental function div_dd(a1, a2) result(b)
+    type(dual), intent(in) :: a1
+    type(dual), intent(in) :: a2
+    type(dual) :: b
+    b%val = a1%val/a2%val
+    b%der = (a1%val*a2%der - a1%der*a2%val)/(a2%val**2)
+  end function
+  
+  pure elemental function div_dr(a1, a2) result(b)
+    type(dual), intent(in) :: a1
+    real(dp), intent(in) :: a2
+    type(dual) :: b
+    b%val = a1%val/a2
+    b%der = a1%der/a2
+  end function
+  
+  pure elemental function div_rd(a1, a2) result(b)
+    real(dp), intent(in) :: a1
+    type(dual), intent(in) :: a2
+    type(dual) :: b
+    b%val = a1/a2%val
+    b%der = a1/a2%der
+  end function
+
+  !~~~ add ~~~!
+  
+  pure elemental function add_dd(a1, a2) result(b)
+    type(dual), intent(in) :: a1
+    type(dual), intent(in) :: a2
+    type(dual) :: b
+    b%val = a1%val + a2%val
+    b%der = a2%der + a1%der
+  end function
+  
+  pure elemental function add_dr(a1, a2) result(b)
+    type(dual), intent(in) :: a1
+    real(dp), intent(in) :: a2
+    type(dual) :: b
+    b%val = a1%val + a2
+    b%der = a1%der
+  end function
+  
+  pure elemental function add_rd(a1, a2) result(b)
+    real(dp), intent(in) :: a1
+    type(dual), intent(in) :: a2
+    type(dual) :: b
+    b = a2 + a1
+  end function
+  
+end module
---
+++ test_forwarddif.f90
@@ -0,0 +1,139 @@
+! module test_mod
+!   use forwarddif, only: dp
+!   implicit none
+
+!   abstract interface
+!     function func_sig(x) result(res)
+!       use forwarddif, only: dual
+!       type(dual), intent(in) :: x
+!       type(dual) :: res
+!     end function
+!   end interface
+  
+! contains
+
+!   function func_normal(x) result(res)
+!     real(dp), intent(in) :: x
+!     real(dp) :: res
+!     res = sin(x + x**2.0_dp)
+!   end function 
+
+!   function func_dual(x) result(res)
+!     use forwarddif
+!     type(dual), intent(in) :: x
+!     type(dual) :: res
+!     res = sin(x + x**2.0_dp)
+!   end function
+
+!   function dfunc_normal(x) result(res)
+!     real(dp), intent(in) :: x
+!     real(dp) :: res
+!     res = (2.0_dp*x + 1.0_dp)*cos(x + x**2.0_dp)
+!   end function  
+
+!   function derivative(func, x) result(dfunc)
+!     use forwarddif, only: dual
+!     procedure(func_sig) :: func
+!     real(dp), intent(in) :: x
+!     real(dp) :: dfunc
+
+!     type(dual) :: ff
+
+!     ff = func(dual(x))
+
+!     dfunc = ff%der
+!   end function
+  
+! end module
+
+program test_forwarddif
+  use forwarddif, only: dual, dp
+  implicit none
+  abstract interface
+    function fcn_normal_sig(x) result(res)
+      import :: dp
+      real(dp), intent(in) :: x
+      real(dp) :: res
+    end function
+  end interface
+
+  call test()
+
+contains
+
+  subroutine test()
+    use forwarddif, only: derivative
+    implicit none
+    real(dp) :: x, fcn_x, dfcn_x
+    integer :: i, n
+    real(dp) :: t(2), tmp, res(2)
+
+    x = 2.0_dp
+    n = 100000000
+
+    ! Autodiff
+    tmp = 0.0_dp
+    call cpu_time(t(1))
+    do i = 1,n
+      call derivative(func_dual, x, fcn_x, dfcn_x)
+      tmp = tmp + fcn_x
+    enddo
+    call cpu_time(t(2))
+    print*,fcn_x, dfcn_x, (t(2) - t(1))/real(n,dp), tmp
+
+    n = 100000000
+
+    tmp = 0.0_dp
+    call cpu_time(t(1))
+    do i = 1,n
+      fcn_x = func_normal(x)
+      dfcn_x = dfunc_normal(x)
+      tmp = tmp + fcn_x
+    enddo
+    call cpu_time(t(2))
+    print*,fcn_x, dfcn_x, (t(2) - t(1))/real(n,dp), tmp
+
+    tmp = 0.0_dp
+    call cpu_time(t(1))
+    do i = 1,n
+      call finite_difference(func_normal, x, 1.0e-4_dp, fcn_x, dfcn_x)
+      tmp = tmp + fcn_x
+    enddo
+    call cpu_time(t(2))
+    print*,fcn_x, dfcn_x, (t(2) - t(1))/real(n,dp), tmp
+
+  end subroutine
+
+  subroutine finite_difference(fcn, x, eps, fcn_x, dfcn_x)
+    use forwarddif, only: fcn_sig
+    procedure(fcn_normal_sig) :: fcn
+    real(dp), intent(in) :: x
+    real(dp), intent(in) :: eps
+    real(dp), intent(out) :: fcn_x
+    real(dp), intent(out) :: dfcn_x
+    real(dp) :: h
+    fcn_x = fcn(x)
+    h = x*eps
+    dfcn_x = (fcn(x+h) - fcn_x)/h
+  end subroutine
+
+  function func_normal(x) result(res)
+    real(dp), intent(in) :: x
+    real(dp) :: res
+    res = sin(x + x**2.0_dp)
+  end function 
+
+  function func_dual(x) result(res)
+    use forwarddif
+    type(dual), intent(in) :: x
+    type(dual) :: res
+    res = sin(x + x**2.0_dp)
+  end function
+
+  function dfunc_normal(x) result(res)
+    real(dp), intent(in) :: x
+    real(dp) :: res
+    res = (2.0_dp*x + 1.0_dp)*cos(x + x**2.0_dp)
+  end function
+  
+end program
